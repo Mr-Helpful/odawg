0adef8075038db3077a5d46d086972771a2e1c6f
Alex Colby <alexanderdcolby@gmail.com>
2024-10-27 23:58:22 +0000

    tweak: adds example from a large (370K words) word file - this effectively demonstrates the minimisation process, reducing a 1M node Trie to a 160K node DAWG

---
+++ examples/large/main.rs
@@ -0,0 +1,27 @@
+use odawg::{utils::from_word, FlatDawg, ReadDawg, WideNode};
+use std::{fs, path::Path};
+
+fn main() {
+  let file_path = Path::new(file!()).parent().unwrap();
+  let words_path = file_path.join("Large.txt");
+  let dawg_path = file_path.join("Large.dawg");
+
+  let content = fs::read_to_string(words_path).expect("Should be able to read words file as text");
+
+  let mut dawg: FlatDawg<WideNode> = content.split("\n").map(|word| from_word(&word)).collect();
+
+  println!("Dawg size = {}", dawg.len());
+  println!("Dawg has {} nodes", dawg.0.len());
+  dawg.unlink();
+  println!("Dawg size = {}", dawg.len());
+  println!("Dawg has {} nodes", dawg.0.len());
+  dawg.minimise();
+  println!("Dawg size = {}", dawg.len());
+  println!("Dawg has {} nodes", dawg.0.len());
+  dawg.trim();
+  println!("Dawg size = {}", dawg.len());
+  println!("Dawg has {} nodes", dawg.0.len());
+
+  let bytes = bincode::serialize(&dawg).expect("serialisation should succeed");
+  fs::write(dawg_path, bytes).expect("Should be able to write file")
+}
